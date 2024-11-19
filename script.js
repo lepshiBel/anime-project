d49644fa942671a2c12257cc9dc192d6f548759c
lepshiBel <eliseyavvakumov@mail.ru>
2024-11-19 18:04:30 +0100

    Added header elements and fonts

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleInfoChange() {
+    const selectElement = document.getElementById('search-info');
+    const selectedInfo = selectElement.value;
+    alert('Вы выбрали: ' + selectedInfo);
+}
+
+function searchImages() {
+    
+}
