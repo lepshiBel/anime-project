3ab09563587e5d1342a109662bf64e0f9e2fa079
lepshiBel <eliseyavvakumov@mail.ru>
2025-01-06 17:38:51 +0100

    Added site adaptive, added footer, reworked cards.

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const baseUrl = 'https://kitsu.io/api/edge';
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
-
 function getSearchInfo() {
     const selectedInfo = document.getElementById('search-info').value;
     return selectedInfo;
@@ -42,58 +37,104 @@ function updateAnimeMangaDom(data) {
     //     data.data.sort((a, b) => a.relationships.chapters.data - b.relationships.chapters.data).forEach(res => console.log(res));
     // }
 
-    const searchResults = document.getElementById('search-results');
-    searchResults.innerHTML = data.data.map(res => {
-        return `
-        <div class="row">
-            <div class="col s12 m7">
-                <div class="card">
-                    <div class="card-image">
-                        <img src="${res.attributes.posterImage.original}">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title">${res.attributes.canonicalTitle}</span>
-                        <p>${res.attributes.synopsis}</p>
-                    </div>
-                    <div class="card-action">
-                        <a href="${res.attributes.tba}">Find out more</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `       
-    }).join('');
+  const searchResults = document.getElementById('search-results');
+
+  const animeAndMangaByCategories = data.data
+    .reduce((acc, anime) => {
+        const { type } = anime;
+        if (acc[type] === undefined) acc[type] = [];
+        acc[type].push(anime);
+        return acc;
+    }, {});
+
+  searchResults.innerHTML = Object.keys(animeAndMangaByCategories).map(key => {
+    const animeAndMangaHTML = animeAndMangaByCategories[key].map(res => {
+          return `
+              <div class="subcontainer">
+                  <div class="card">
+                      <div class="card-image">
+                          <img src="${res.attributes.posterImage.original}">
+                      </div>
+                      <div class="card-content">
+                          <span class="card-title"><b>${res.attributes.canonicalTitle}</b></span>
+                          <p class="card-description">${res.attributes.synopsis}</p>
+                      </div>
+                      <div class="card-action">
+                          <a href="${res.attributes.tba}">Find out more</a>
+                      </div>
+                  </div>
+              </div>
+          `
+      }).join('');
+      
+    return `
+        <section>
+            <h3>${key.toUpperCase()}</h3>
+            <div class="created-row">${animeAndMangaHTML}</div>
+        </section>
+    `
+  }).join(''); 
 }
 
+// if (getSearchInfo() === 'anime') {
+//     data.data.sort((a, b) => a.relationships.episodes.data - b.relationships.episodes.data).forEach(res => console.log(res));
+// }
+// else if (getSearchInfo() === 'manga') {
+//     data.data.sort((a, b) => a.relationships.chapters.data - b.relationships.chapters.data).forEach(res => console.log(res));
+// }
 function updateCharactersDom(data) {
-    // if (getSearchInfo() === 'anime') {
-    //     data.data.sort((a, b) => a.relationships.episodes.data - b.relationships.episodes.data).forEach(res => console.log(res));
-    // }
-    // else if (getSearchInfo() === 'manga') {
-    //     data.data.sort((a, b) => a.relationships.chapters.data - b.relationships.chapters.data).forEach(res => console.log(res));
-    // }
+  const searchResults = document.getElementById('search-results');
 
-    const searchResults = document.getElementById('search-results');
-    searchResults.innerHTML = data.data.map(res => {
-        return `
-        <div class="row">
-            <div class="col s12 m7">
-                <div class="card">
-                    <div class="card-image">
-                        <img src="${res.attributes.image}">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title">${res.attributes.name}</span>
-                        <p>${res.attributes.description}</p>
-                    </div>
-                    <div class="card-action">
-                        <a href="${res.attributes.createdAt}">Find out more</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `        
+  const charactersByCategories = data.data
+    .reduce((acc, character) => {
+      const { type } = character;
+      if (acc[type] === undefined) acc[type] = [];
+      acc[type].push(character);
+      return acc;
+    }, {});
+
+  searchResults.innerHTML = Object.keys(charactersByCategories).map(key => {
+    const charactersHTML = charactersByCategories[key].map(res => {
+      let image = getImageIfExists(res);
+
+      return `
+          <div class="subcontainer">
+              <div class="card">
+                  <div class="card-image">
+                      <img src="${image}" alt="${image}">
+                  </div>
+                  <div class="card-content">
+                      <span class="card-title"><b>${res.attributes.name}</b></span>
+                      <p class="card-description">${res.attributes.description}</p>
+                  </div>
+                  <div class="card-action">
+                      <a href="${res.attributes.createdAt}">Find out more</a>
+                  </div>
+              </div>
+          </div>
+      `
     }).join('');
+
+    return `
+        <section>
+            <h3>${key.toUpperCase()}</h3>
+            <div class="created-row">${charactersHTML}</div>
+        </section>
+    `
+  }).join(''); 
+}
+
+function getImageIfExists(res) {
+  const images = res.attributes.image;
+  let image = null;
+
+  if (images === null) {
+    image = "Image not found";
+  } else {
+    image = images.original;
+  }
+
+  return image;
 }
 
 function pageLoaded() {
@@ -102,3 +143,12 @@ function pageLoaded() {
 }
 
 window.addEventListener('load', pageLoaded);
+
+
+function objToString(obj) {
+  let str = '';
+  for (const [p, val] of Object.entries(obj)) {
+    str += `${p}::${val}\n`;
+  }
+  return str;
+}
